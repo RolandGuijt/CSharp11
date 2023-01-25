a39f69f40729b013bc2be7f920d40fe5e1a721ba
Roland Guijt <roland.guijt@gmail.com>
2023-01-25 16:29:40 +0100

    Add UTF8

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+var someString = @"abc\d";
+
+var language = "en";
+var htmlTag = $"""<html lang="{language}"/>""";
+Console.WriteLine(htmlTag);
+
+//{
+//  "Logging": {
+//      "LogLevel": {
+//          "Default": "Information"
+//      }
+//  }
+//}
+
+var logLevel = "Information";
+var json = $$"""
+    {
+      "Logging": {
+          "LogLevel": {
+              "Default": "{{logLevel}}"
+          }
+      }
+    }
+    """;
+Console.WriteLine(json);
